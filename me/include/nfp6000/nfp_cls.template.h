faf81d41179ca92b85671ff287615fcfaddff8a1
mrapson <devnull@localhost>
2014-03-20 14:10:56 +0200

    me/include: Add template for ME cluster header file - Provide ring defines (not yet used) - Provide autopush defines (have been used) - Offsets only for: hash, IM, TRNG, NFA, EM   Note that EM has a separate header nfp_event.h supplying further defines
    Signed-off-by: Michael Rapson <michael.rapson@netronome.com>
    Acked-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

---
+++ me/include/nfp6000/nfp_cls.template.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014,  Netronome Systems, Inc.  All rights reserved.
+ *
+ * @file          nfp6000/nfp_cls.h
+ * @brief         NFP6000 CLS CSR definitions
+ */
+
+#ifndef _NFP6000__NFP_CLS_H_
+#define _NFP6000__NFP_CLS_H_
+
+pyexec<set_output_mode("nfp")>
+
+/*
+ * CLS CSRs
+ */
+pyexec<dump_map(ClusterScratchCppMap(altname="CLS"), docs=True)>
+
+/*
+ * CLS register structures
+ */
+#if defined(__NFP_LANG_MICROC)
+
+pyexec<dump_map_structs(ClusterScratchCppMap(altname="CLS"))>
+
+#endif /* __NFP_LANG_MICROC */
+
+#endif /* !_NFP6000__NFP_CLS_H_ */
