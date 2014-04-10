7bdd5731102ff8c37a3eb11ac9cb10fd6d81a3d0
Michael Rapson <michael.rapson@netronome.com>
2014-04-10 13:44:20 +0200

    me/lib: add initial files for standard library
    Signed-off-by: Michael Rapson <michael.rapson@netronome.com>
    
    Acked-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

---
+++ me/lib/std/libstd.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2012-2014,  Netronome Systems, Inc.  All rights reserved.
+ *
+ * @file          lib/std/libstd.c
+ * @brief         Standard library for MicroC
+ */
+
+#ifndef _STD__LIBSTD_C_
+#define _STD__LIBSTD_C_
+
+/*
+ * The following files implement all the funcitonality in <std/*.h>.
+ */
+
+
+#endif /* !_STD__LIBSTD_C_ */
+
