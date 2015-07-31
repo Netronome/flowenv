857ae880d64b89cdd13dfad9b8fdcc7a622f63bf
Brian Auth <brian.auth@netronome.com>
2015-07-31 14:45:46 -0400

    me/lib/pktdma: Library to provide safe wrappers around PE DMA functions
    Signed-off by: Brian Auth <brian.auth@netronome.com>
    Acked-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

---
+++ me/lib/pktdma/libpktdma.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2014-2015,  Netronome Systems, Inc.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file        /lib/pktdma/libpktdma.c
+ * @brief       Memory Unit Packet Engine DMA with semaphore protection
+ */
+
+#include <nfp.h>
+#include <stdint.h>
+#include <types.h>
+
+#include <std/synch.h>
+#include <nfp/mem_pe.h>
+#include <nfp/mem_atomic.h>
+
+/* You may have no more than 16 outstanding DMA commands per CTM. */
+#define DMA_CMDS_PER_CTM   16
+
+SEM_CLS_DECLARE(libpktdma_sem, DMA_CMDS_PER_CTM);
+
+__intrinsic void
+__pktdma_mu_to_ctm(__ctm void* ctm_addr, __mem void* mem_addr, size_t size,
+                   uint32_t poll_int)
+{
+    SEM_WAIT(libpktdma_sem, poll_int);
+
+    mem_pe_dma_mu_to_ctm(ctm_addr, mem_addr, size);
+
+    SEM_POST(libpktdma_sem);
+}
+
+__intrinsic void
+pktdma_mu_to_ctm(__ctm void* ctm_addr, __mem void* mem_addr, size_t size)
+{
+    __pktdma_mu_to_ctm(ctm_addr, mem_addr, size, SYNCH_SEM_DEFAULT_POLL);
+}
+
+__intrinsic void
+__pktdma_ctm_to_mu(__mem void* mem_addr, __ctm void* ctm_addr, size_t size,
+                   uint32_t poll_int)
+{
+    SEM_WAIT(libpktdma_sem, poll_int);
+
+    mem_pe_dma_ctm_to_mu(mem_addr, ctm_addr, size);
+
+    SEM_POST(libpktdma_sem);
+}
+
+__intrinsic void
+pktdma_ctm_to_mu(__mem void* mem_addr, __ctm void* ctm_addr, size_t size)
+{
+    __pktdma_ctm_to_mu(mem_addr, ctm_addr, size, SYNCH_SEM_DEFAULT_POLL);
+}
---
+++ me/lib/pktdma/pktdma.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2015,  Netronome Systems, Inc.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file          lib/pktdma/pktdma.h
+ * @brief         Memory Unit Packet Engine DMA with semaphore protection
+ */
+#ifndef _PKTDMA__PKTDMA_H_
+#define _PKTDMA__PKTDMA_H_
+
+#include <nfp.h>
+#include <stdint.h>
+#include <types.h>
+
+/**
+ * DMA from MU to CTM with semaphore protection.
+ *
+ * @param ctm_addr  32-bit pointer to the CTM start address
+ * @param mem_addr  40-bit pointer to the MU start address
+ * @param size      Size of the DMA, must be a multiple of 64B and <= 2048B
+ * @param poll_int  Cycles to wait if no credits are available
+ *
+ * @note poll_int must be less than 0x00100000 (1<<20), see sleep().
+ *       pktdma_mu_to_ctm() uses SYNCH_SEM_DEFAULT_POLL.  Use
+ *       __pktdma_mu_to_ctm() and increase the interval if there is high
+ *       contention for credits.
+ * @note Use the real-address mode for ctm_addr, not the packet-address mode
+ */
+__intrinsic void
+__pktdma_mu_to_ctm(__ctm void* ctm_addr, __mem void* mem_addr, size_t size,
+                   uint32_t poll_int);
+
+__intrinsic void
+pktdma_mu_to_ctm(__ctm void* ctm_addr, __mem void* mem_addr, size_t size);
+
+/**
+ * DMA from CTM to MU with semaphore protection.
+ *
+ * @param mem_addr  40-bit pointer to the MU start address
+ * @param ctm_addr  32-bit pointer to the CTM start address
+ * @param size      Size of the DMA, must be a multiple of 64B and <= 2048B
+ * @param poll_int  Cycles to wait if no credits are available
+ *
+ * @note poll_int must be less than 0x00100000 (1<<20), see sleep().
+ *       pktdma_ctm_to_mu() uses SYNCH_SEM_DEFAULT_POLL.  Use
+ *       __pktdma_ctm_to_mu() and increase the interval if there is high
+ *       contention for credits.
+ * @note Use the real-address mode for ctm_addr, not the packet-address mode
+ */
+__intrinsic void
+__pktdma_ctm_to_mu(__mem void* mem_addr, __ctm void* ctm_addr, size_t size,
+                   uint32_t poll_int);
+
+__intrinsic void
+pktdma_ctm_to_mu(__mem void* mem_addr, __ctm void* ctm_addr, size_t size);
+
+#endif  /*_PKTDMA__PKTDMA_H_*/
